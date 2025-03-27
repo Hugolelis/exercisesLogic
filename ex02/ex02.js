5ed83b01b035f93f1bbe194690c74e7224bffbb4
Hugo De Lelis <hugodelelis05@gmail.com>
2025-03-27 13:38:42 -0300

    feat(ex02) 🎉: Encrypt a text

---
+++ ex02/ex02.js
@@ -0,0 +1,20 @@
+/* Challenge: Encrypt a text based on its position in the alphabet */
+
+function criptografa(text) {
+    const alfabeto = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+    
+    const textFormated = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+    const textSplited = textFormated.split('')
+
+    let cripto = textSplited.map(char => {
+        if (char == ' ') return ' '
+        return alfabeto.indexOf(char)
+    })
+    
+    console.log(cripto.join(''))
+}
+
+criptografa('arroz com feijão')

2332ff5a87611840825e90a2c0916968f8f03dae
Hugo De Lelis <hugodelelis05@gmail.com>
2025-03-28 11:14:28 -0300

    feat(ex04) 🎉: return hours in seconds

--- js/ex04/ex04.js
+++ js/ex04/ex04.js
@@ -1,1 +1,14 @@
 /* challenge: Return hour in seconds */
+
+function hourInSeconds(hours) {
+    const [h, m, s] = hours.split(':').map(Number)
+    
+    const result = (3600 * h) + (60 * m) + s
+
+    return console.log(result)
+}
+
+const hours = "11:04:10"
+
+hourInSeconds(hours)
+
